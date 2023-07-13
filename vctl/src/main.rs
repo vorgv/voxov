b82d56e1ad4523051d51e3fc39c45fe48b2ac83f
Celio Grand <celiogrand@outlook.com>
2023-07-14 04:58:26 +0800

    Rewrite parser in clap

--- vctl/src/main.rs
+++ vctl/src/main.rs
@@ -1,61 +1,46 @@
-use std::io;
+use clap::{Parser, Subcommand};
+use std::process::exit;
 use std::str::FromStr;
 use voxov::auth::nspm;
 use voxov::config::Config;
 use voxov::database::namespace::SMSSENT;
 use voxov::database::Database;
-use voxov::message::id::Id;
+use voxov::message::Id;
 use voxov::to_static;
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() {
     let c = to_static!(Config::new());
-    let db = to_static!(Database::new(c).await);
-    let mut line_buffer = String::new();
-    loop {
-        line_buffer.clear();
-        io::stdin().read_line(&mut line_buffer)?;
-        let command = Command::new(&line_buffer, c, db);
-        let output = command.execute().await;
-        println!("{}", output);
+    let db: &Database = to_static!(Database::new(c).await);
+    let cli = Cli::parse();
+    let result = match cli.command {
+        Command::Sent { from, to, message } => {
+            let message = Id::from_str(format!("{:0>32}", message).as_str()).unwrap();
+            let s = nspm(SMSSENT, &to, &message);
+            db.set(&s[..], from, c.access_ttl).await
+        }
+        _ => todo!(),
+    };
+    if result.is_err() {
+        exit(1);
     }
 }
 
-struct Command {
-    argv: Vec<String>,
-    config: &'static Config,
-    db: &'static Database,
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
 }
 
-impl Command {
-    fn new(s: &String, config: &'static Config, db: &'static Database) -> Self {
-        Command {
-            argv: s.split_whitespace().map(String::from).collect(),
-            config,
-            db,
-        }
-    }
-    async fn execute(&self) -> String {
-        if self.argv.is_empty() {
-            return "".to_string();
-        }
-        match &self.argv[0] {
-            s if s == "sent" => {
-                let phone = &self.argv[1];
-                println!("'{}'", &self.argv[2]);
-                let message = Id::from_str(format!("{:0>32}", self.argv[2]).as_str()).unwrap();
-                let user_phone = &self.argv[3];
-                let s = nspm(SMSSENT, &phone, &message);
-                match self
-                    .db
-                    .set(&s[..], user_phone, self.config.access_ttl)
-                    .await
-                {
-                    Ok(_) => "Ok".to_string(),
-                    Err(e) => e.to_string(),
-                }
-            }
-            unknown => format!("Unknown command: {}", unknown),
-        }
-    }
+#[derive(Subcommand)]
+pub enum Command {
+    /// MESSAGE recieved from FROM to TO.
+    Sent {
+        from: String,
+        to: String,
+        message: String,
+    },
+    /// Add credit to UID
+    AddCredit { uid: String, credit: u64 },
 }
