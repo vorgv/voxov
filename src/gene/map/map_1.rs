b9df7d6a4655d6d31a2237ee03a05c99339c0bb5
Celio Grand <celiogrand@outlook.com>
2023-07-26 17:37:57 +0800

    WIP map

--- src/gene/map/map_1.rs
+++ src/gene/map/map_1.rs
@@ -42,11 +42,12 @@ use crate::message::{Costs, Id, Int, Uint};
 struct Insert {
     _type: String,
     // Id is managed by database.
+    // Uid is managed by auth.
 
     // Pub is managed by censor.
     #[serde(with = "ts_seconds_option")]
     _eol: Option<DateTime<Utc>>,
-    _tips: Option<Int>,
+    _tip: Option<Int>,
     // Size is counted by backend.
     _ns: Option<String>,
 
@@ -54,11 +55,6 @@ struct Insert {
     _1: Option<Value>,
     _2: Option<Value>,
     _3: Option<Value>,
-
-    _0_: Option<Value>,
-    _1_: Option<Value>,
-    _2_: Option<Value>,
-    _3_: Option<Value>,
 
     _geo: Option<Vec<f64>>,
 
@@ -70,15 +66,24 @@ struct Insert {
 struct Query {
     _type: String,
     _id: Option<ObjectId>,
-    _uid: String,
+    _uid: Option<String>,
 
-    #[serde(default)]
-    _pub: bool,
+    /// Default: public and self.
+    _pub: Option<bool>,
+
     #[serde(with = "ts_seconds_option")]
     _eol: Option<DateTime<Utc>>,
-    _tips: Option<Int>,
+    #[serde(with = "ts_seconds_option")]
+    _eol_: Option<DateTime<Utc>>,
+
+    _tip: Option<Int>,
+    _tip_: Option<Int>,
+
     _size: usize,
+    _size_: usize,
+
     _ns: Option<String>,
+    _ns_: Option<String>,
 
     _0: Option<Value>,
     _1: Option<Value>,
@@ -97,9 +102,6 @@ struct Query {
 
     /// Selected fields.
     _v: Option<Vec<String>>,
-
-    #[serde(flatten)]
-    v: Map<String, Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -110,7 +112,7 @@ struct Update {
     // Pub is managed by censor.
     #[serde(with = "ts_seconds_option")]
     _eol: Option<DateTime<Utc>>,
-    _tips: Option<Int>,
+    _tip: Option<Int>,
     // Size is counted by backend.
     _ns: Option<String>,
 
@@ -118,11 +120,6 @@ struct Update {
     _1: Option<Value>,
     _2: Option<Value>,
     _3: Option<Value>,
-
-    _0_: Option<Value>,
-    _1_: Option<Value>,
-    _2_: Option<Value>,
-    _3_: Option<Value>,
 
     _geo: Option<Vec<f64>>,
 
@@ -154,9 +151,59 @@ pub async fn v1(
 ) -> Result<String, Error> {
     let request: Request = serde_json::from_str(arg).map_err(|e| Error::ParseJson(e))?;
     match request {
-        Request::Insert(insert) => todo!(),
-        Request::Query(query) => todo!(),
-        Request::Update(update) => todo!(),
-        Request::Delete(delete) => todo!(),
+        Request::Insert(insert) => {
+            // Set uid.
+            // Set pub to false.
+            // Calculate eol -> ttl -> space.
+            // Check tip & space.
+            // Check ns. Reserve "_*".
+            // Insert indexed keys.
+            // Insert geo (length is 2)
+            // Insert other fields. Reserve "_*".
+            // Count & Try removing from space.
+            // Insert document with deadline.
+            // Reply
+            todo!()
+        }
+        Request::Query(query) => {
+            // Set id.
+            // Set uid.
+            // Set pub.
+            // Set eol.
+            // Set tip.
+            // Set size.
+            // Set ns.
+            // Set keys.
+            // Set max doc count.
+            // Set geo.
+            // Select fields.
+            // Query document with deadline.
+            // Reply.
+            todo!()
+        }
+        Request::Update(update) => {
+            // Set id.
+            // Set uid.
+            // Get original doc eol & size.
+            // Set eol.
+            // Set tip.
+            // Set ns.
+            // Set keys.
+            // Set geo.
+            // Set user fields.
+            // Count new size.
+            // Calculate diff between eol and size, then update space.
+            // Update document with deadline.
+            // Reply.
+            todo!()
+        }
+        Request::Delete(delete) => {
+            // Set id.
+            // Set uid.
+            // Calculate eol & size -> space.
+            // Delete document with deadline.
+            // Add space to changes & reply.
+            todo!()
+        }
     }
 }
