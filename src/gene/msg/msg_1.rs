f46498d6f3f3eec883986d7e57f9024a97a478b8
Celio Grand <celiogrand@outlook.com>
2023-08-26 12:06:20 +0800

    Impl gene msg sent

--- src/gene/msg/msg_1.rs
+++ src/gene/msg/msg_1.rs
@@ -9,7 +9,7 @@
 //! No public flag needed, but TO can report.
 
 use crate::{
-    database::{namespace::UID2PHONE, ns},
+    database::{namespace::UID2CREDIT, ns},
     error::Error,
     gene::map,
     ir::Id,
@@ -26,8 +26,11 @@ const NS: &str = "_chan";
 const FROM: &str = "_0";
 const TO: &str = "_1";
 const SENT: &str = "_2";
+const SENT_: &str = "_2_";
 const READ: &str = "_3";
+const READ_: &str = "_3_";
 const TIP: &str = "_4";
+const TIP_: &str = "_4_";
 const TYPE: &str = "_5";
 const VALUE: &str = "value";
 
@@ -43,12 +46,18 @@ struct Send {
 
 #[derive(Deserialize, Debug)]
 struct Sent {
+    id: Option<ObjectId>,
+    eol: DateTime<Utc>,
+    eol_: DateTime<Utc>,
     to: Option<String>,
     sent: Option<DateTime<Utc>>,
     sent_: Option<DateTime<Utc>>,
     read: Option<DateTime<Utc>>,
     read_: Option<DateTime<Utc>>,
     tip: Option<i64>,
+    tip_: Option<i64>,
+    size: Option<i64>,
+    size_: Option<i64>,
     r#type: Option<String>,
     n: Option<u64>,
 }
@@ -115,14 +124,17 @@ pub async fn v1(mut cx: map::V1Context<'_>) -> Result<String> {
             }
 
             let to = Id::try_from(&request.to)?;
-            let u2c = ns(UID2PHONE, &to);
+            let u2c = ns(UID2CREDIT, &to);
+
             if db.exits(&u2c[..]).await? < 1 {
                 return Err(Error::AuthInvalidUid);
             }
 
-            if request.tip < 0 {
+            if 0 < request.tip || request.tip < cx.changes.tip {
                 return Err(Error::CostTip);
             }
+
+            db.incrby(&u2c[..], request.tip).await?;
 
             let arg = json!({
                 "_type": "Put",
@@ -139,7 +151,33 @@ pub async fn v1(mut cx: map::V1Context<'_>) -> Result<String> {
             cx.arg = &arg;
             map::v1(cx, true).await
         }
-        Request::Sent(request) => todo!(),
+
+        Request::Sent(request) => {
+            let arg = json!({
+                "_type": "Get",
+                "_id": request.id,
+                "_uid": cx.uid.to_string(),
+                "_eol": request.eol,
+                "_eol_": request.eol_,
+                "_ns": NS,
+                TO: request.to,
+                SENT: request.sent,
+                SENT_: request.sent_,
+                READ: request.read,
+                READ_: request.read_,
+                TIP: request.tip,
+                TIP_: request.tip_,
+                "_size": request.size,
+                "_size_": request.size_,
+                TYPE: request.r#type,
+                "_n": request.n,
+            })
+            .to_string();
+
+            cx.arg = &arg;
+            map::v1(cx, true).await
+        }
+
         Request::Receive(request) => todo!(),
         Request::Read(request) => todo!(),
         Request::Unread(request) => todo!(),
