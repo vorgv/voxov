01907775d8444de21119ad5ac21e63d24a1da661
Celio Grand <celiogrand@outlook.com>
2023-09-02 22:08:17 +0800

    Test session end

--- tests/auth_test.rs
+++ tests/auth_test.rs
@@ -52,11 +52,95 @@ async fn session_refresh() {
         .await
         .unwrap();
 
-    assert_ne!(response.headers().get("access").unwrap().to_str().unwrap(), access);
+    assert_ne!(
+        response.headers().get("access").unwrap().to_str().unwrap(),
+        access
+    );
 }
 
 #[tokio::test]
-async fn session_end() {}
+async fn session_end() {
+    // End access
+    let response = _post()
+        .await
+        .header("type", "AuthSessionStart")
+        .send()
+        .await
+        .unwrap();
+
+    let headers = response.headers();
+    let access = headers.get("access").unwrap().to_str().unwrap();
+    let refresh = headers.get("refresh").unwrap().to_str().unwrap();
+
+    let response = _post()
+        .await
+        .header("type", "AuthSessionEnd")
+        .header("access", access)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.headers().get("type").unwrap().to_str().unwrap(),
+        "AuthSessionEnd"
+    );
+
+    let response = _post()
+        .await
+        .header("type", "AuthSessionEnd")
+        .header("access", access)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.headers().get("type").unwrap().to_str().unwrap(),
+        "Error"
+    );
+
+    let response = _post()
+        .await
+        .header("type", "AuthSessionRefresh")
+        .header("refresh", refresh)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.headers().get("type").unwrap().to_str().unwrap(),
+        "AuthSessionRefresh"
+    );
+    // End access and refresh
+
+    let access = response.headers().get("access").unwrap().to_str().unwrap();
+
+    let response = _post()
+        .await
+        .header("type", "AuthSessionEnd")
+        .header("access", access)
+        .header("refresh", refresh)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.headers().get("type").unwrap().to_str().unwrap(),
+        "AuthSessionEnd"
+    );
+
+    let response = _post()
+        .await
+        .header("type", "AuthSessionRefresh")
+        .header("refresh", refresh)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        response.headers().get("type").unwrap().to_str().unwrap(),
+        "Error"
+    );
+}
 
 #[tokio::test]
 async fn session_sms() {}
